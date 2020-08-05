640e29c4d64ad446b08c5235924cc6920f49ac65
BeemerCodes <detonainyoutub@gmail.com>
2020-08-05 15:07:57 -0300

    correção de repositorios

---
+++ Aulas.js
@@ -0,0 +1,16 @@
+/*
+window.alert('Bem vindo!!')
+window.confirm('Esta treinando vagal?')
+
+var nome = window.prompt('Qual seu nome?')
+window.alert("é um prazer te conhecer " + nome + "!!")
+
+
+var numero1 = Number(window.prompt("Digite um numero: "))
+var numero2 = Number(window.prompt("Digite outro numero: "))
+window.alert("A soma de " + numero1 + " mais " + numero2 + " gerou " + (numero1+numero2))
+
+var nome = window.prompt("Qual é seu nome? ")
+document.write(`Seu nome tem ${nome.length} letras!`)
+
+*/
